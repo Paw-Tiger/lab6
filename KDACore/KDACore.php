f7b2e475322ddf9e5a8d6293861f81158e2d3516
Dmitiy Kalnichenko <kalnichenko1996@mail.ru>
2016-12-22 11:44:37 +0200

    first commit

---
+++ KDACore/KDACore.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace KDAWebLab;
+
+/**
+ * Core - test core app class
+ * @author Andrey Lisnyak <munspel@ukr.net>
+ */
+class KDACore {
+
+    public function __construct() {
+        echo "Kalnichenko Dmitriy 5133";
+    }
+}
